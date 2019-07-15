f508a14f51b3d9029486192436841a0d519fe399
Rodrigo Lourenço <rsilvape@gmail.com>
2019-07-14 21:20:06 -0300

    Página inicial para repositórios

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Repository from "./pages/Repository";
 
 const AppRouter = () => (
   <Router>
     <Switch>
-      <Route path="/" component={Home} />
+      <Route path="/" exact component={Home} />
+      <Route path="/:user" component={Repository} />
     </Switch>
   </Router>
 );
